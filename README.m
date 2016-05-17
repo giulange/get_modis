e8cdde54a21e3d1d7f938cd7888873d8e669c3a0
giulange <gyuliano@libero.it>
2016-05-17 08:51:55 +0200

    version used to download 2001-2016 MODIS Terra images for Italy

---
+++ README.m
@@ -0,0 +1,38 @@
+% The modis package under MatLab was developed to manage Satellite data
+% coming from MODIS.
+% There are three scripts each with own parameters to be configured. In
+% order of execution there are:
+%   (1) downmodis.m   :: It is based on the get_modis.py script included in
+%                        the folder. It downloads hdf files and stores them
+%                        in a user defined path. The user can select tiles
+%                        accordindg to a ROI, years and start/end DOY.
+%                        fnc() ––> get_modis.py
+%                        pars  ––> product, start/end DOY, MODIS platform,
+%                                  years, tiles
+% 
+%   (2) mosaicmodis.m :: It extracts from hdf files the required band (e.g.
+%                        NDVI or EVI) and stitches together the tiles of
+%                        the same DOY. The user can select year, band,
+%                        tiles, start/end DOY.
+%                        The output are .vrt files, one for each complete
+%                        DOY required by user.
+%                        fnc() ––> gdalinfo_getBandName=@gdalinfo()
+%                                  gdalbuildvrt_createMosaic=@gdalbuildvrt()
+%                        pars  ––> product, start/end DOY, years, tiles,
+%                                  band
+% 
+%   (3) convertmodis.m:: It converts the .vrt files into GTiff (or other
+%                        format as required) applying the required
+%                        reference system. The developer is planning to set
+%                        an option with which create yearly stacks for a
+%                        ROI.
+%                        fnc() ––> gdalinfo_getBandName=@gdalinfo()
+%                                  gdalbuildvrt_createMosaic=@gdalbuildvrt()
+%                        pars  ––> product, start/end DOY, years, tiles,
+%                                  band
+% 
+% The gdal package should be already installed on the computer, typically
+% at:
+%   ll /usr/bin/*gdal*
+%   ll /usr/local/bin/*gdal*
+
