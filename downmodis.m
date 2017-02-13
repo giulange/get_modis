f3531c59f64898fdcae86ab2d0db5a5a3b3d1dc9
giulange <gyuliano@libero.it>
2017-02-13 18:42:08 +0100

    updated downmodis after user & pswd added to get_modis.py by Jose Gomez-Dans

--- downmodis.m
+++ downmodis.m
@@ -9,12 +9,14 @@ PRODUCT     = 'MOD13Q1.006';
 DIR_OUT     = '/media/DATI/db-backup/MODIS/hdf-it/';
 SDOY        = 1;
 EDOY        = 366;
+USER        = 'giulange';
+PSWD        = 'XMa-q9t-pTt-dZC';
 %% -- PARRAMETRIC
 YEARS       = 2016:2017; %2003:2016;
 TILES       = {'h18v04','h18v05','h19v04','h19v05'};
 %% original command
 % !./get_modis.py -v -s MOLT -p MOD13Q1.006 -y 2004 -t h18v04 -o ~/Downloads/modis#2/ -b 1 -e 366
-getmodis = @(y,t) ['!~/git/get_modis/./get_modis.py -v -s ',PLATFORM,' -p ',PRODUCT,' -y ',num2str(y),' -t ',t,' -o ',DIR_OUT,' -b ',num2str(SDOY),' -e ',num2str(EDOY)];
+getmodis = @(y,t) ['!~/git/get_modis/./get_modis.py -u ',USER,' -P ',PSWD,' -v -s ',PLATFORM,' -p ',PRODUCT,' -y ',num2str(y),' -t ',t,' -o ',DIR_OUT,' -b ',num2str(SDOY),' -e ',num2str(EDOY)];
 %% batch download
 
 for y=1:numel(YEARS)
